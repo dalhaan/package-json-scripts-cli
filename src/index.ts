9168961c8643d1299e70658eb76a825fb2088dfc
dalhaan <dallan.freemantle@gmail.com>
2022-11-01 00:03:44 +1300

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -8,34 +8,44 @@ import inquirerPrompt from 'inquirer-autocomplete-prompt';
 
 inquirer.registerPrompt('autocomplete', inquirerPrompt);
 
+// Get scripts object from package.json
 const rawJson = readFileSync('./package.json', 'utf8');
 const packageJson: IPackageJson = JSON.parse(rawJson);
+const scriptsObj = packageJson.scripts;
 
-const scripts = packageJson.scripts;
-
-if (!scripts) {
+if (!scriptsObj) {
   console.log('No scripts found.');
   process.exit();
 }
 
+// Get scripts
+const scripts = Object.keys(scriptsObj);
+
+// CLI prompts
 inquirer
   .prompt([
     {
       type: 'autocomplete',
       name: 'script',
       message: 'Select script to run:',
-      choices: Object.keys(scripts),
+      choices: scripts,
       source: (answersSoFar: never, input?: string) =>
+        // Seach scripts for input
         new Promise((resolve) => {
           if (!input) {
-            return resolve(Object.keys(scripts));
+            return resolve(scripts);
           }
-          resolve(Object.keys(scripts).filter((s) => s.startsWith(input)));
+          resolve(scripts.filter((s) => s.startsWith(input)));
         }),
     },
   ])
   .then(({ script }: { script: string }) => {
+    // Spawn child process to execute script
     spawn('yarn', ['run', script], {
+      // Binds:
+      //   - the current process stdin to the child process stdin (for interactivity if the child process requires user input)
+      //   - the child process stdout to the current process stdout (to stream the output of the script)
+      //   - the child process stderr to the current process stderr (to stream the errors of the script)
       stdio: [process.stdin, process.stdout, process.stderr],
     });
   });
